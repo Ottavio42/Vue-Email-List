fae6ccfda080a11e94a204a9b39f1a783e6cb350
Ottavio42 <ottaviog88@gmail.com>
2021-09-27 17:39:52 +0200

    fix-e-fine-esercizio

--- js/script.js
+++ js/script.js
@@ -6,5 +6,10 @@ var app = new Vue({
     
     methods: {
     },
-    
+    mounted(){
+        for (let i = 0; i < 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then(response => (this.emails.push(response.data.response)))
+        }
+    } 
   })
